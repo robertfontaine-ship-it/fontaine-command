9dd2ce433f19f45961f511520a9f9f8aca673e89
robertfontaine-ship-it <robert.fontaine@nn.k12.va.us>
2026-07-13 23:41:54 -0400

    Normalize Fashion sales role-play success criteria

---
+++ fashion-mp2-selling-quality-patch.js
@@ -0,0 +1,4 @@
+(()=>{
+ const lesson=lessons.find(item=>item.id==="FASH-033");
+ if(lesson)lesson.success="I will know I am successful when I can complete the selling process professionally and meet every role-play criterion with at least 80% accuracy.";
+})();
