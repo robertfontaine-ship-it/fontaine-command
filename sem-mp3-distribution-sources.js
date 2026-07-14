66b5c1a8b16e0db95c39daa0e57f485dddb2894d
robertfontaine-ship-it <robert.fontaine@nn.k12.va.us>
2026-07-14 01:36:18 -0400

    Add verified SEM MP3 Distribution Power Move source

---
+++ sem-mp3-distribution-sources.js
@@ -0,0 +1,11 @@
+(()=>{
+ const source=window.FONTaineMP3Sources;
+ if(!source)return;
+ source.resources["sem-distribution-power-move"]={
+  title:"Distribution Power Move Template",
+  course:"SEM",
+  type:"Slides",
+  fileId:"1Yiw3lLiO65iOnKJ2eSL2NdUN1jcVdQvhzX1r2rFf5Vk",
+  lessonIds:["SEM-043","SEM-045","SEM-046","SEM-047"]
+ };
+})();
