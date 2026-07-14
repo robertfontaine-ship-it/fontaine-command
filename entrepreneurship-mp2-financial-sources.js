6c07865335bbe9ee1a3df448baa54c625245b351
robertfontaine-ship-it <robert.fontaine@nn.k12.va.us>
2026-07-14 00:28:23 -0400

    Add verified Entrepreneurship MP2 financial sources

---
+++ entrepreneurship-mp2-financial-sources.js
@@ -0,0 +1,8 @@
+(()=>{
+ const source=window.FONTaineMP2Sources;
+ if(!source)return;
+ Object.assign(source.resources,{
+  "ent-financial-slides":{title:"Financial Aspects of Business",course:"Entrepreneurship",type:"Slides",fileId:"1gX7ylIDOKghr8C3GThL1EWuNAK5GXJyMVPOuThSf1gg",lessonIds:["ENT-036","ENT-037","ENT-038","ENT-039","ENT-040"]},
+  "ent-business-plan-template":{title:"Business Plan Template",course:"Entrepreneurship",type:"PDF",fileId:"1NMqCqQ6rRBV7xHUhVKsu6TH0CNl_vSZ8",lessonIds:["ENT-036","ENT-037","ENT-038","ENT-039"]}
+ });
+})();
