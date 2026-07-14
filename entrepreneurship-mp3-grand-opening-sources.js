d8efae4e987d5b00f7b8447c4058aa140fa63401
robertfontaine-ship-it <robert.fontaine@nn.k12.va.us>
2026-07-14 02:53:43 -0400

    Add verified Entrepreneurship MP3 Grand Opening source

---
+++ entrepreneurship-mp3-grand-opening-sources.js
@@ -0,0 +1,11 @@
+(()=>{
+ const source=window.FONTaineMP3Sources;
+ if(!source)return;
+ source.resources['ent-grand-opening-kit']={
+  title:'Grand Opening Marketing Kit',
+  course:'Entrepreneurship',
+  type:'Slides',
+  fileId:'1emMzXY2FJVS_FGCIgbMIQ1DG69jzUs5XfqrcAp5mHUg',
+  lessonIds:['ENT-049','ENT-050']
+ };
+})();
